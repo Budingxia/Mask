b9ac09292b74d7ff54cd21e5bf11bb4536ffb68b
Budingxia <55691800+Budingxia@users.noreply.github.com>
2020-12-06 10:02:42 +0800

    Create UserServlet.java

---
+++ interface/src/servlet/UserServlet.java
@@ -0,0 +1,49 @@
+package com.xmx.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.xmx.model.User;
+import com.xmx.service.UserService;
+
+/**
+ *视图层
+ * 用户控制器
+ */
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	   doPost(request,response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf8");
+		String action=request.getParameter("action");//取得操作行为
+		String userid=request.getParameter("userid");//获取用户id
+		
+	
+	}
+
+}
